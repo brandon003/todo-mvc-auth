628c3e5d44a398112a19ee68fe5fd7f6bb7b8223
Brandon Lobo <brandon_slobo@hotmail.com>
2022-09-06 14:04:31 -0500

    commented out old code related to mark in/complete

--- public/js/main.js
+++ public/js/main.js
@@ -37,43 +37,43 @@ async function deleteTodo() {
   }
 }
 
-async function markComplete() {
-  const todoId = this.parentNode.dataset.id;
-  console.log('todoId:',todoId);
-
-  try {
-    const response = await fetch("todos/markComplete", {
-      method: "put",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        todoIdFromJSFile: todoId,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-    location.reload();
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-async function markIncomplete() {
-  const todoId = this.parentNode.dataset.id;
-  try {
-    const response = await fetch("todos/markIncomplete", {
-      method: "put",
-      headers: { "Content-type": "application/json" },
-      body: JSON.stringify({
-        todoIdFromJSFile: todoId,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-    location.reload();
-  } catch (err) {
-    console.log(err);
-  }
-}
+// async function markComplete() {
+//   const todoId = this.parentNode.dataset.id;
+//   console.log('todoId:',todoId);
+// 
+//   try {
+//     const response = await fetch("todos/markComplete", {
+//       method: "put",
+//       headers: { "Content-type": "application/json" },
+//       body: JSON.stringify({
+//         todoIdFromJSFile: todoId,
+//       }),
+//     });
+//     const data = await response.json();
+//     console.log(data);
+//     location.reload();
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// 
+// async function markIncomplete() {
+//   const todoId = this.parentNode.dataset.id;
+//   try {
+//     const response = await fetch("todos/markIncomplete", {
+//       method: "put",
+//       headers: { "Content-type": "application/json" },
+//       body: JSON.stringify({
+//         todoIdFromJSFile: todoId,
+//       }),
+//     });
+//     const data = await response.json();
+//     console.log(data);
+//     location.reload();
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 /*
 <img class="spaceship cruiserX3" src="shipX3.png"
